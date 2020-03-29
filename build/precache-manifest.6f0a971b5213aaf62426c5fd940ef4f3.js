0ff9b3f989d2d0e6643961b4f9eb318cc01f54a3
John Williams <john@insidenewcity.com>
2020-03-29 15:35:27 -0400

    Base cra setup

---
+++ build/precache-manifest.6f0a971b5213aaf62426c5fd940ef4f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d653764d1b12faa8fab94f781ecee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe6a92a5426b768170c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a59be833a37cd25d401a",
+    "url": "/static/js/2.bc84b4ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc84b4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe6a92a5426b768170c",
+    "url": "/static/js/main.5cc57411.chunk.js"
+  },
+  {
+    "revision": "986b5d26645148e781ef",
+    "url": "/static/js/runtime-main.1a204d75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
